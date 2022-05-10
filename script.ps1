6b5b3bbbf9ec1f130d42465b74fa00a3df9145d3
Walker <devin.walker@thomsonreuters.com>
2022-05-10 13:54:39 -0500

    Updating file

--- script.ps1
+++ script.ps1
@@ -8,7 +8,7 @@ Function ShowStorageContainer
     # Iterate containers, display properties
     Foreach ($container in $containers) 
     {
-        $blobs = Get-AzStorageBlob -Context $context -Container $container.Name
+        $blobs = Get-AzStorageBlob -Context $Context -Container $container.Name
         Write-Host $container.Name "found..."
     }
 
@@ -22,7 +22,7 @@ ShowStorageContainer
 ## Function to delete a container  
 Function DeleteStorageContainer  
 {  
-    Write-Host -ForegroundColor Green "Deleting the storage container ($container.Name)"       
+    Write-Host -ForegroundColor Green "Deleting the storage container $container.Name"       
     ## Delete a container  
     Remove-AzStorageContainer -Container $container.Name -Context $Context
 }
